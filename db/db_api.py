2054230ce5494135773eaa6473337e82dacbb9b4
Adam Czajka <czajka.adam147@gmail.com>
2021-01-06 18:26:34 +0100

    Add db_api

--- db/db_api.py
+++ db/db_api.py
@@ -1,52 +1,81 @@
 import pathlib
 import sqlite3
 import pandas as pd
-
-
 DB_FILE = pathlib.Path(__file__).resolve().parent.joinpath("DatabaseName.db").resolve()
-def create_connection(db_file): 
-    """ create a database connection to the 
-    SQLite database specified by 
-    db_file :param db_file: 
-    database file :return: 
-    Connection object or None """ 
-    
-    conn = None 
-    try: conn = sqlite3.connect(db_file) 
-    except Error as e: 
-        print(e) 
-    return conn
+DB_CONN = None
+try:
+    DB_CONN = sqlite3.connect(str(DB_FILE))
+except Error as err:
+    print(err)
 
-def get_wind_data(start, end):
-    """
-    Query wind data rows between two ranges
-
-    :params start: start row id
-    :params end: end row id
-    :returns: pandas dataframe object
-    """
-
-    con = sqlite3.connect(str(DB_FILE))
-    statement = f'SELECT Speed, SpeedError, Direction FROM Wind WHERE rowid > "{start}" AND rowid <= "{end}";'
-    df = pd.read_sql_query(statement, con)
+def select_sensor():
+    statement = f'SELECT * FROM sensors;'
+    df = pd.read_sql_query(statement, DB_CONN)
     return df
 
+def insert_sensor(name, anomaly, value, id_trace, id):
+    sensor = (name, anomaly, value, id_trace, id)
+    statement = f'INSERT INTO sensors (name, anomaly, value, id_trace, id) VALUES (?, ?, ?, ?, ?);'
+    cur = DB_CONN.cursor()
+    cur.execute(statement, sensor)
+    DB_CONN.commit()
+    return cur.lastrowid
 
-def get_wind_data_by_id(id):
-    """
-    Query a row from the Wind Table
-
-    :params id: a row id
-    :returns: pandas dataframe object
-    """
-
-    con = sqlite3.connect(str(DB_FILE))
-    statement = f'SELECT * FROM Wind WHERE rowid = "{id}";'
-    df = pd.read_sql_query(statement, con)
+def select_people():
+    statement = f'SELECT * FROM people;'
+    df = pd.read_sql_query(statement, DB_CONN)
     return df
 
-def create_tab_sensors_if_not_exists(id, id_trace, name, anomaly, value):
- conn = sqlite3.connect(str(DB_FILE))
- statement = f'CREATE TABLE IF NOT EXISTS sensors (id PRIMARY KEY NOT NULL, id_trace TEXT NOT NULL, name TEXT, anomaly INTEGER, value REAL);'
- df = pd.read_sql_query(statement, conn)
- return df
+def insert_person(name, surname, birth_year, disabled, id):
+    person = (name, surname, birth_year, disabled, id)
+    statement = f'INSERT INTO people (name, surname, birth_year, disabled, id) VALUES (?, ?, ?, ?, ?);'
+    cur = DB_CONN.cursor()
+    cur.execute(statement, person)
+    DB_CONN.commit()
+    return cur.lastrowid
+
+def select_traces():
+    statement = f'SELECT * FROM traces;'
+    df = pd.read_sql_query(statement, DB_CONN)
+    return df
+
+def insert_trace(name, date, id_person, id):
+    trace = (name, date, id_person, id)
+    statement = f'INSERT INTO traces (name, date, id_person, id) VALUES (?, ?, ?, ?);'
+    cur = DB_CONN.cursor()
+    cur.execute(statement, trace)
+    DB_CONN.commit()
+    return cur.lastrowid
+
+
+if __name__ == "__main__":
+# TESTING SENSORS TABLE
+    res = select_sensor()
+    print('SENSORS:\n', res)
+    res = insert_sensor('L1', 0, 3.14, 0, 0)
+    print('INSERT SENSOR:\n', res, sep=' ')
+    res = select_sensor()
+    print('SENSORS:\n', res)
+    print()
+
+# TESTING PEOPLE TABLE
+    res = select_people()
+    print('PEOPLE:\n', res)
+    res = insert_person('Adam', 'Czajka', '1998', 0, 1)
+    print('INSERT PERSON:\n', res, sep=' ')
+    res = select_people()
+    print('PEOPLE:\n', res)
+    print()
+
+# TESTING TRACES TABLE
+    res = select_traces()
+    print('TRACES:\n', res)
+    res = insert_trace('Trace name', 'date', 0, 1)
+    print('INSERT TRACE:\n', res, sep=' ')
+    res = select_traces()
+    print('TRACES:\n', res)
+    print()
+
+
+
+
